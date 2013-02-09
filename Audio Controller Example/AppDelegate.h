d60bb0b5fc974dfc6f44fcf8d591f31b382c3408
Kevin Murphy <kpmurphy91@gmail.com>
2013-02-09 05:10:18 -0500

    Initial Commit

---
+++ Audio Controller Example/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Audio Controller Example
+//
+//  Created by Kevin Murphy on 2/9/13.
+//  Copyright (c) 2013 Kevin Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Audio Controller Example/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Audio Controller Example
+//
+//  Created by Kevin Murphy on 2/9/13.
+//  Copyright (c) 2013 Kevin Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
